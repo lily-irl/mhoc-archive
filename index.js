855ec708623275ed78470329688836988ee78f96
Lily Hail <83549667+lily-irl@users.noreply.github.com>
2024-02-08 00:21:09 -0600

    change default port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const session = require('express-session')
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3300
 
 const api = require('./routes/api')
 const auth = require('./routes/auth')
